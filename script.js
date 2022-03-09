bcc876d8bf2099edc9151877eb169a7dfcc5c58b
hamitovm <hamitovm@gmail.com>
2022-03-10 00:59:15 +0300

    Sum without highest and lowest number - BP

--- script.js
+++ script.js
@@ -12,3 +12,12 @@ function sumArray(array) {
       }
       return sum - minmax[0] - minmax[1];
   }
+// Sum without highest and lowest number (best practices - 1)
+sumArray = a => a ? a.sort((x, y) => x - y).slice(1, -1).reduce((s, e) => s + e, 0) : 0
+
+// Sum without highest and lowest number (best practices - 2)
+function sumArray(array) {
+    return Array.isArray(array) && array.length > 1
+      ? array.reduce((s, n) => s + n, 0) - Math.min(...array) - Math.max(...array)
+      : 0
+  }
